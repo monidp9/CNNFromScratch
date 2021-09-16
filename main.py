8decb5f6bdc3c0eb662a0770f33716e0582139b8
monidp9 <monteirodelprete@gmail.com>
2021-09-16 16:56:36 +0200

    testing nel main

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from net import Net
 import numpy as np
 
-print(np.array([1,2,3]))
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def identity(x):
+    return x
+
+def sum_of_square():
+    pass
+
+
+net = Net(hidden_layers_num=1,
+          nodes_num=[2],
+          activation_functions=[sigmoid, identity],
+          error_function=sum_of_square)
+
+
+x = np.random.normal(size=(5, 3))
+layer_input, layer_output = net.forwardStep(x)
+
+print(layer_input[1])

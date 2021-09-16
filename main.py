a96b84d7e3290cd68fca8710f1881eb012b397ac
monidp9 <monteirodelprete@gmail.com>
2021-09-16 18:03:42 +0200

    test del metodo forwardStep con immagine del dataset

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import utility
+import numpy as np
+import matplotlib.pyplot as plt
+
+from net import Net
 from mnist import MNIST
+
+def identity(x):
+    return x
+
 
 mndata = MNIST('./python-mnist/data')
 images, labels = mndata.load_training()
 
-utility.get_configuration_net()
+# utility.get_configuration_net()
 
-print('\n\n\n')
+image = images[0]
+image = np.array(image)
+
+
+# creazione rete
+net = Net(hidden_layers_num=1,
+          nodes_num=[3],
+          activation_functions=[identity, identity],
+          error_function=[identity])
+
+x = image.reshape(1, -1)
+
+input, output = net.forwardStep(x)
+
+print(output)

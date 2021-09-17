adce61bf5e761768974bcd0d1aea98d6c3c42fd6
monidp9 <monteirodelprete@gmail.com>
2021-09-17 17:48:27 +0200

    aggiunta file .gitignore

--- learning.py
+++ learning.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def back_progagation(net, x, t):    # non sto considerando bias
-    
+
     layer_input, layer_output = net.forwardStep(x)
 
     delta = []
@@ -12,12 +12,14 @@ def back_progagation(net, x, t):    # non sto considerando bias
 
     for i in range(net.n_layers):
         delta.append(np.zeros(net.n_layers))
-    
-    for i in range(net.n_layers-1,-1,-1):
+
+    for i in range(net.n_layers-1, -1, -1):
         if i == net.n_layers-1 :
-            delta[i] = net.act_fun_deriv_per_layer[i](layer_input[i]) * net.error_fun_deriv(layer_output[i])
+            delta[i] = net.act_fun_deriv_per_layer[i](layer_input[i]) * \
+                       net.error_fun_deriv(layer_output[i])
         else :
-            delta[i] = net.act_fun_deriv_per_layer[i](layer_input[i]) * np.dot(np.transpose(net.weights[i+1]),delta[i+1])
+            delta[i] = net.act_fun_deriv_per_layer[i](layer_input[i]) * \
+                       np.dot(np.transpose(net.weights[i+1]),delta[i+1])
 
     for i in range(net.n_layers):
         print(i)
@@ -25,5 +27,3 @@ def back_progagation(net, x, t):    # non sto considerando bias
             weights_deriv.append(np.dot(delta[i],np.transpose(x)))
         else :
             weights_deriv.append(np.dot(delta[i],np.transpose(layer_output[i-1])))
-
-

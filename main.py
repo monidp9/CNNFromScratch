eb592352daad069ce9e23555a9fa17c36fd16d4a
monidp9 <monteirodelprete@gmail.com>
2021-09-17 12:34:01 +0200

    eliminazione metodi in net.py

--- main.py
+++ main.py
@@ -5,32 +5,82 @@ import matplotlib.pyplot as plt
 from net import Net
 from mnist import MNIST
 
-<<<<<<< HEAD
-def identity(x):
-    return x
+
+def convolution(image):
+    kernel = [[-1, -1, -1], [-1, 2, -1], [-1, -1, -1]]
+    kernel = np.array(kernel)
+
+    n_rows = image.shape[0]
+    n_columns = image.shape[1]
+
+    new_image = list()
+    temp_result = list()
+
+    for x in range(1, n_rows - 1):
+        for y in range(1, n_columns - 1):
+            row_start = x - 1
+            row_finish = row_start + (kernel.shape[0])
+            column_start = y - 1
+            column_finish = column_start + (kernel.shape[1])
+
+            region = image[row_start:row_finish, column_start:column_finish]
+
+            result = np.multiply(region, kernel)
+            result = np.sum(result)
+
+            temp_result.append(result)
+
+        new_image.append(temp_result.copy())
+        temp_result[:] = []
+
+    return np.array(new_image)
+
+
 
 
 mndata = MNIST('./python-mnist/data')
 images, labels = mndata.load_training()
-=======
-# mndata = MNIST('./python-mnist/data')
-# images, labels = mndata.load_training()
->>>>>>> e96108e937076f1fe4bf13082e1bd43525e61bd8
 
 # utility.get_configuration_net()
+
+
 
 image = images[0]
 image = np.array(image)
 
+'''
+TEST CONVOLUZIONE
+x = image.reshape(28, 28)
+conv_x = convolution(x)
+
+fig = plt.figure(figsize=(1, 2))
+fig.add_subplot(1, 2, 1)
+plt.imshow(x)
+
+fig.add_subplot(1, 2, 2)
+plt.imshow(conv_x)
+
+plt.show()
+'''
+
+
+def identity(x):
+    return x
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
 
 # creazione rete
-net = Net(hidden_layers_num=1,
-          nodes_num=[3],
-          activation_functions=[identity, identity],
+net = Net(n_hidden_layers=1,
+          n_hidden_nodes=[3],
+          activation_functions=[sigmoid, identity],
           error_function=[identity])
 
-x = image.reshape(1, -1)
+image = image.reshape(-1, 1)
+# input, output = net.forwardStep(image)
 
-input, output = net.forwardStep(x)
-
-print(output)
+x = np.array([2000, 3000, -100])
+print('x', x)
+print('sig', sigmoid(x))

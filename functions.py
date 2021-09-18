ffdfe3ad0ae29bd0ea8d98e1ce6cf602ebec1757
monidp9 <monteirodelprete@gmail.com>
2021-09-18 11:08:18 +0200

    scrittura funzioni di errore e derivate completata

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+def softmax(scores):
+    return np.exp(scores) / np.sum(np.exp(scores))
+
+
 # funzioni di attivazione
 def identity(x):
     return x
@@ -9,14 +13,6 @@ def sigmoid(x):
 
 def relu(x):
     return np.maximum(x, 0)
-
-
-# funzioni di errore
-def sum_of_squares(pred, label):
-    return 0.5 * np.sum(np.power(pred - label, 2))
-
-def cross_entropy(pred, label):
-    return - np.sum(label * np.log(pred))
 
 
 # derivate funzioni di attivazione
@@ -34,6 +30,18 @@ def relu_deriv(x):
     return y
 
 
+# funzioni di errore
+def sum_of_squares(pred, label):
+    return 0.5 * np.sum(np.power(pred - label, 2))
+
+def cross_entropy(pred, label):
+    return - np.sum(label * np.log(pred))
+
+def cross_entropy_softmax(pred, label):
+    softmax_pred = softmax(pred)
+    return cross_entropy(softmax_pred, label)
+
+
 # derivate funzioni di errore
 def sum_of_squares_deriv(pred, label):
     return pred - label
@@ -41,10 +49,14 @@ def sum_of_squares_deriv(pred, label):
 def cross_entropy_deriv(pred, label):
     return - pred / label
 
+def cross_entropy_softmax_deriv(pred, label):
+    softmax_pred = softmax(pred)
+    return softmax_pred - label
+
 
 
 activation_functions = [sigmoid, identity, relu]
 activation_functions_deriv= [sigmoid_deriv, identity_deriv, relu_deriv]
 
-error_functions = [cross_entropy, sum_of_squares]
-error_functions_deriv = [cross_entropy_deriv, sum_of_squares_deriv]
+error_functions = [cross_entropy, cross_entropy_softmax, sum_of_squares]
+error_functions_deriv = [cross_entropy_deriv, cross_entropy_softmax_deriv, sum_of_squares_deriv]

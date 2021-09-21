23b64ade078cdf1ba8140693bc9d3cecffb7b2ea
Salvatore Amodio <salvatoreamodio@MBPdiSalvatore.home-life.hub>
2021-09-21 11:24:06 +0200

    creazione funzioni elementari convolutional_net

--- convolutional_net.py
+++ convolutional_net.py
@@ -1,7 +1,59 @@
+import numpy as np
+import functions as fun
+from numpy.core.shape_base import _block_check_depths_match
+
 
 class ConvolutionalNet:
-    def __init__(self):
-        pass
+    def __init__(self, n_conv_layers, n_kernels_per_layers, n_nodes_hidden_layer, act_fun_codes, error_fun_code):
+        self.n_input_nodes = 784 # dipende dal dataset: 784
+        self.n_output_nodes = 10 # dipende dal dataset: 10
+
+        self.n_conv_layers = n_conv_layers 
+        self.n_kernels_per_layers = n_kernels_per_layers.copy()
+
+        self.KERNEL_SIZE = 3 # si presuppongono kernel uguali di dimensione quadrata
+        self.STRIDE = 1         # S: spostamento
+        self.PADDING = 1         # P: padding 
+
+
+        self.act_fun_codes = act_fun_codes.copy()
+        self.nodes_per_layer = list()
+        self.nodes_per_layer.append(n_nodes_hidden_layer)
+        self.nodes_per_layer.append(self.n_output_nodes)        
+
+        self.error_fun_code = error_fun_code
+
+        self.weights = list()
+        self.bias = list()
+        self.kernels = list()
+
+        self.__initialize_weights_and_bias()
+        self.__initialize_kernels()
+
+    def __initialize_kernels(self): 
+        for i in range(self.n_conv_layers):
+            self.kernels.append(np.random.uniform(size=(self.KERNEL_SIZE, self.KERNEL_SIZE, 
+                                                        self.n_kernels_per_layers[i])))
+
+
+    def __initialize_weights_and_bias(self):
+        for i in range(2):
+            if i == 0:
+                input = 0 # costruire funzione che calcola input 
+
+                #last_conv_layer_size = self.__get_feature_map_size(input,self.KERNEL_SIZE,self.PADDING,self.STRIDE)
+                
+                self.weights.append(np.random.uniform(size=(self.nodes_per_layer[i],
+                                                      input)))
+            else:
+                self.weights.append(np.random.uniform(size=(self.nodes_per_layer[i],
+                                                     self.nodes_per_layer[i-1])))
+
+            self.bias.append(np.random.uniform(size=(self.nodes_per_layer[i], 1)))
+
+ 
+    def __get_feature_map_size(w,F,P,S): # w : dimensione input
+        return ((w - F + 2*P) / S) + 1    
 
     def convolution(self):
         pass
@@ -11,3 +63,8 @@ class ConvolutionalNet:
 
     def max_pooling(self):
         pass
+
+    def print_config(self):
+        pass
+
+
--- main_sasy.py
+++ main_sasy.py
@@ -7,7 +7,7 @@ import utility
 import numpy as np
 import matplotlib.pyplot as plt
 
-from net import Net
+from convolutional_net import ConvolutionalNet
 from mnist import MNIST
 
 
@@ -19,22 +19,20 @@ X = utility.get_mnist_data(X)
 t = utility.get_mnist_labels(t)
 
 
-net = Net(n_hidden_layers=3,
-          n_hidden_nodes_per_layer=[10,10,10],
-          act_fun_codes= [0,0,0,1],
-          error_fun_code=1)
+net = ConvolutionalNet(n_conv_layers = 3, n_kernels_per_layers = [3,3,3], 
+                       n_nodes_hidden_layer = 3, act_fun_codes = [0,0], error_fun_code = [0])
+  
 net.print_config()
 
 X,t = utility.get_random_dataset(X,t,20)
-
 X_train, X_test, t_train, t_test = utility.train_test_split(X,t,0.25)
 
 
+'''
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
 X,t = load_iris(return_X_y=True)
-
 
 X_train, X_test, y_train, y_test = train_test_split(X, t)
 X_train = np.transpose(X_train)
@@ -44,54 +42,13 @@ X_test = np.transpose(X_test)
 y_test = utility.get_iris_labels(y_test)
 
 net = batch_learning(net, X_train, t_train, X_test, y_test)
-
-
-
-
-
-
-
 '''
 
-from sklearn.datasets import load_iris
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import log_loss
-
-n_samples = X_train.shape[0]
-
-t_train = np.array(t_train)
-one_hot_labels = np.zeros((t_train.shape[0], 3))
-
-for n in range(t_train.shape[0]):
-    label = t_train[n] - 1
-    one_hot_labels[n][label] = 1
-
-t_train = one_hot_labels
-
-
-t_test = np.array(t_test)
-one_hot_labels = np.zeros((t_test.shape[0], 3))
-
-for n in range(t_test.shape[0]):
-    label = t_test[n] - 1
-    one_hot_labels[n][label] = 1
-
-t_test = one_hot_labels
 
 
 
-clf = MLPClassifier(solver='sgd', alpha=1e-5,
-                        hidden_layer_sizes=(200, 200), random_state=0, learning_rate_init=0.1, activation='relu', batch_size=n_samples, max_iter=500)
 
 
-clf.fit(X_train,t_train)
-
-predicted = clf.predict_proba(X_test)
-
-print(log_loss(t_test,predicted))
-
-'''
 
 
 

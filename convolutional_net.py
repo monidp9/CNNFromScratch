99f5a8787d19ec837ab04f29d37ec7b52b3caacf
monidp9 <monteirodelprete@gmail.com>
2021-09-21 09:45:32 +0200

    inizio rete convoluzionale

---
+++ convolutional_net.py
@@ -0,0 +1,13 @@
+
+class ConvolutionalNet:
+    def __init__(self):
+        pass
+
+    def convolution(self):
+        pass
+
+    def forward_step(self):
+        pass
+
+    def max_pooling(self):
+        pass

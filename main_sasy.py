bdba6d21064b37ee43f486c6a774d046dbb60517
Salvatore Amodio <salvatoreamodio@MBPdiSalvatore.home-life.hub>
2021-09-23 16:54:32 +0200

    implementazione discesa del gradiente per RPROP + divisione organizzata in funzioni di RPROP

--- main_sasy.py
+++ main_sasy.py
@@ -18,14 +18,12 @@ X = utility.get_mnist_data(X)
 t = utility.get_mnist_labels(t)
 
 
-n_conv_layers, n_kernels_per_layer, n_hidden_nodes, act_fun_codes, error_fun_code = utility.get_configuration_conv_net()
-
-net = ConvolutionalNet(n_conv_layers = n_conv_layers, n_kernels_per_layer = n_kernels_per_layer,
-                       n_hidden_nodes = 3, act_fun_codes = act_fun_codes, error_fun_code = error_fun_code)
+net = ConvolutionalNet(n_conv_layers = 2, n_kernels_per_layer = [2,3,4],
+                       n_hidden_nodes = 5, act_fun_codes = [1,1], error_fun_code = 1)
 
 net.print_config()
 
 X,t = utility.get_random_dataset(X,t,20)
 X_train, X_test, t_train, t_test = utility.train_test_split(X,t,0.25)
 
-# net = conv_batch_learning(net, X_train, t_train, X_test, t_test)
+net = conv_batch_learning(net, X_train, t_train, X_test, t_test)
